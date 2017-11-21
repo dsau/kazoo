371d035ba876752f920949156ce353aa70c4db89
lazedo <luis.azedo@factorlusitano.com>
2017-11-21 12:42:36 +0000

    avoid api validation error

--- applications/sysconf/src/sysconf_get.erl
+++ applications/sysconf/src/sysconf_get.erl
@@ -59,4 +59,5 @@ get_value(Category, Key, Default, Node) ->
 
 -spec maybe_fix_undefined(any()) -> any().
 maybe_fix_undefined('undefined') -> <<"undefined">>;
+maybe_fix_undefined([]) -> <<"undefined">>;
 maybe_fix_undefined(Value) -> Value.

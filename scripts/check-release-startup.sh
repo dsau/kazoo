7a7d341b065975202b4cd6c65c9492b8b10a76b9
lazedo <luis.azedo@factorlusitano.com>
2018-02-28 03:05:21 +0000

    check release script tweaks

--- scripts/check-release-startup.sh
+++ scripts/check-release-startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
-PATH=$PATH:$(dirname $0):/opt/kazoo/_rel/kazoo/bin
-export PATH
-
 [[ ! -d _rel ]] && echo 'Cannot find _rel/ Is the release built?' && exit -1
 
-echo "Checking release startup with node $rel..."
+echo "Checking release startup..."
+
+REL_DIR=$(dirname $0)/../_rel/kazoo
+PATH=$PATH:$(dirname $0):${REL_DIR}/bin
+export PATH
+LOG_DIR=${REL_DIR}/log
+export LOG_DIR
+mkdir -p ${LOG_DIR}
 
 shutdown() {
     sup init stop
@@ -16,14 +20,15 @@ TIMEOUT=${1:-"120"}
 SEARCH_TERM=${2:-"nada"}
 echo "waiting for '$SEARCH_TERM'"
 (timeout --foreground $TIMEOUT tail -f _rel/kazoo/log/debug.log  2>&1 &) | grep -q "$SEARCH_TERM" && echo "found '$SEARCH_TERM'" && return 0
+echo "timeout waiting for '$SEARCH_TERM'" > ${LOG_DIR}/error.log
 echo "timeout waiting for '$SEARCH_TERM'"
 return 1
 }
 
 
 script() {
-    mkdir -p _rel/kazoo/log
-    touch _rel/kazoo/log/debug.log
+    touch ${LOG_DIR}/debug.log
+    touch ${LOG_DIR}/error.log
     waitfor 180 "auto-started kapps" || shutdown
     echo "creating account"
     sup crossbar_maintenance create_account 'compte_maitre' 'royaume' 'superduperuser' 'pwd!' || shutdown
@@ -41,9 +46,9 @@ script() {
 export KAZOO_NODE=apps
 export KAZOO_COOKIE=change_me
 
-script > _rel/kazoo/log/sup.log 2>&1 &
+script > _rel/kazoo/log/sup.log 2>${LOG_DIR}/error.log &
 
-export KAZOO_CONFIG=$PWD/rel/ci.config.ini
+export KAZOO_CONFIG=$(dirname $0)/../rel/ci.config.ini
 kazoo console
 
 code=$?
@@ -53,7 +58,7 @@ if [[ -f erl_crash.dump ]]; then
     code=3
 fi
 
-error_log="$PWD/_rel/kazoo/log/error.log"
+error_log="${LOG_DIR}/error.log"
 if [[ -f $error_log ]]; then
     echo
     echo Error log:
